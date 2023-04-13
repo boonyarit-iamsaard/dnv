5419d33d38c307d4e325581f95f4e823c40efd65
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-13 12:09:51 +0700

    build: cleanup home page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import { type AppType } from 'next/app';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+import { type AppType } from 'next/app';
+import Head from 'next/head';
 
 import { api } from '@/utils/api';
 
@@ -12,6 +13,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>DNV</title>
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
