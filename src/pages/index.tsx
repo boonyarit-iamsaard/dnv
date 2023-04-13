c5cf50f0a6c91e648fab78ff3da880195e9d4a44
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-13 12:15:25 +0700

    build: add welcome section background image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { type NextPage } from 'next';
+import Image from 'next/image';
+
+import welcome from '@/assets/images/welcome.jpg';
 
 const Home: NextPage = () => {
   return (
     <main>
-      <section className="grid h-screen place-items-center bg-gradient-to-b from-black/75 to-black/50 text-center text-white">
+      <section className="relative grid h-screen place-items-center bg-gradient-to-b from-black/75 to-black/50 text-center text-white">
+        <Image
+          src={welcome}
+          fill
+          alt="Welcome to Dragonfly Naturist Village"
+          className="-z-10 object-cover"
+        />
         <div className="w-full max-w-screen-sm space-y-4">
           <p>Welcome to</p>
           <h1 className="font-serif text-4xl font-bold uppercase">
