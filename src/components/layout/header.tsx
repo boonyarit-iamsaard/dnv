fcf181c9f48a79957b7a15aa54effa21c639779d
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-14 18:11:37 +0700

    build: add hamburger menu

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,8 +4,10 @@ import Link from 'next/link';
 
 import { cn } from '@/utils/class-names';
 import { debounce } from '@/utils/debounce';
+import { IconMenu2, IconX } from '@tabler/icons-react';
 
 export const Header: FC = () => {
+  const [opened, setOpened] = useState(false);
   const [transparent, setTransparent] = useState(true);
   const [visible, setVisible] = useState(true);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
@@ -42,11 +44,11 @@ export const Header: FC = () => {
         visible ? 'top-0' : '-top-16'
       )}
     >
-      <div className="mx-auto flex w-full max-w-screen-lg items-center px-4 md:px-8">
+      <div className="mx-auto flex w-full max-w-screen-lg items-center justify-between px-4 md:px-8">
         <Link
           href="/"
           className={cn(
-            'flex px-4 py-2 font-serif text-2xl font-bold uppercase leading-none transition-all duration-300 ease-in-out focus:outline-none focus:ring-2',
+            'block px-4 py-2 font-serif text-2xl font-bold uppercase leading-none transition-all duration-300 ease-in-out focus:outline-none focus:ring-2',
             transparent
               ? 'text-white hover:bg-white/20 focus:ring-white'
               : 'text-slate-900 hover:bg-slate-100 focus:ring-slate-900'
@@ -54,6 +56,19 @@ export const Header: FC = () => {
         >
           DNV
         </Link>
+        <button
+          type="button"
+          onClick={() => setOpened(!opened)}
+          className={cn(
+            'z-30 rounded-full p-2 transition-all duration-300 ease-in-out focus:outline-none focus:ring-2',
+            transparent
+              ? 'text-white hover:bg-white/20 focus:ring-white'
+              : 'text-slate-900 hover:bg-slate-100 focus:ring-slate-900'
+          )}
+        >
+          <span className="sr-only">Toggle menu</span>
+          {opened ? <IconX /> : <IconMenu2 />}
+        </button>
       </div>
     </header>
   );
