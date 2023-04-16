9695baf66bc1bf178bd1132b1d7fc0f0b22ab4db
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-16 18:34:18 +0700

    refactor: reorganize navigation component

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,83 +1,44 @@
-import { type FC, type ReactNode } from 'react';
+import { type FC } from 'react';
 
 import Link from 'next/link';
 
 import { LINKS } from '@/constants/links';
-import { useWindowSize } from '@/hooks/use-window-size';
 import { useLayoutStore } from '@/store/layout';
 import { cn } from '@/utils/class-names';
-
-interface NavigationProps {
-  children: ReactNode;
-  opened: boolean;
-  transparent: boolean;
-}
-
-const SmallScreenNavigation: FC<
-  Pick<NavigationProps, 'children' | 'opened'>
-> = ({ children, opened }) => {
-  return (
-    <nav
-      className={cn(
-        'fixed inset-0 flex transform items-center justify-center bg-black py-16 transition-all duration-500 ease-in-out supports-[backdrop-filter]:bg-black/80 supports-[backdrop-filter]:backdrop-blur-md',
-        opened
-          ? 'pointer-events-auto translate-y-0 opacity-100'
-          : 'pointer-events-none -translate-y-full opacity-0'
-      )}
-    >
-      {children}
-    </nav>
-  );
-};
-
-const LargeScreenNavigation: FC<Pick<NavigationProps, 'children'>> = ({
-  children,
-}) => {
-  return <nav className="flex items-center gap-4">{children}</nav>;
-};
-
-const NavigationList: FC<Pick<NavigationProps, 'opened' | 'transparent'>> = ({
-  opened,
-  transparent,
-}) => {
-  return (
-    <ul className="flex flex-col items-center gap-4 md:flex-row md:gap-0">
-      {LINKS.map(({ name, href }) => (
-        <li key={name}>
-          <Link
-            href={href}
-            className={cn(
-              "relative block px-4 py-2 uppercase leading-none after:absolute after:inset-x-4 after:bottom-0 after:block after:h-[0.125rem] after:scale-x-0 after:transition-all after:duration-300 after:ease-in-out after:content-[''] hover:after:scale-x-100 focus:after:scale-x-100",
-              opened || transparent
-                ? 'text-white after:bg-white'
-                : 'text-black after:bg-black'
-            )}
-          >
-            {name}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  );
-};
 
 export const Navigation: FC = () => {
   const {
     header: { transparent },
     drawer: { opened },
   } = useLayoutStore();
-  const { width } = useWindowSize();
 
-  const largeScreen = !!width && width >= 900;
-  const navigationList = (
-    <NavigationList opened={opened} transparent={transparent} />
-  );
-
-  return largeScreen ? (
-    <LargeScreenNavigation>{navigationList}</LargeScreenNavigation>
-  ) : (
-    <SmallScreenNavigation opened={opened}>
-      {navigationList}
-    </SmallScreenNavigation>
+  return (
+    <nav
+      // TODO: Consider cleaning up the class names
+      className={cn(
+        'fixed inset-0 flex transform items-center justify-center bg-black transition-all duration-500 ease-in-out supports-backdrop-blur-md:bg-black/80 supports-backdrop-blur-md:backdrop-blur-md md:pointer-events-auto md:static md:transform-none md:bg-transparent md:py-0 md:opacity-100 md:transition-none md:supports-backdrop-blur-md:bg-transparent md:supports-backdrop-blur-md:backdrop-filter-none',
+        opened
+          ? 'pointer-events-auto translate-y-0 opacity-100'
+          : 'pointer-events-none -translate-y-full opacity-0'
+      )}
+    >
+      <ul className="flex flex-col items-center gap-4 md:flex-row md:gap-0">
+        {LINKS.map(({ name, href }) => (
+          <li key={name}>
+            <Link
+              href={href}
+              className={cn(
+                "relative block px-4 py-2 uppercase leading-none after:absolute after:inset-x-4 after:bottom-0 after:block after:h-[0.125rem] after:scale-x-0 after:transition-all after:duration-300 after:ease-in-out after:content-[''] hover:after:scale-x-100 focus:after:scale-x-100",
+                opened || transparent
+                  ? 'text-white after:bg-white'
+                  : 'text-black after:bg-black'
+              )}
+            >
+              {name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
   );
 };
