2f436c6902637ba7e5d75f1eb71321332e60ad53
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-17 21:00:14 +0700

    build: add navigation link active state

--- src/components/layout/navigation.tsx
+++ src/components/layout/navigation.tsx
@@ -1,12 +1,15 @@
 import { type FC } from 'react';
 
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import { LINKS } from '@/constants/links';
 import { useLayoutStore } from '@/store/layout';
 import { cn } from '@/utils/class-names';
 
 export const Navigation: FC = () => {
+  const { pathname } = useRouter();
+
   const {
     header: { transparent },
     drawer: { opened },
@@ -29,6 +32,7 @@ export const Navigation: FC = () => {
               href={href}
               className={cn(
                 "relative block px-4 py-2 uppercase leading-none after:absolute after:inset-x-4 after:bottom-0 after:block after:h-[0.125rem] after:scale-x-0 after:transition-all after:duration-300 after:ease-in-out after:content-[''] hover:after:scale-x-100 focus:after:scale-x-100",
+                pathname === href ? 'after:scale-x-100' : '',
                 opened || transparent
                   ? 'text-white after:bg-white'
                   : 'text-black after:bg-black'
