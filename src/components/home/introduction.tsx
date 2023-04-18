6fcbe790f515e45ea52956225616acc3d340de62
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-18 21:56:30 +0700

    style: improve introduction section

--- src/components/home/introduction.tsx
+++ src/components/home/introduction.tsx
@@ -1,8 +1,7 @@
 import { type FC } from 'react';
 
-import { IconPhoneCall, IconPhoto } from '@tabler/icons-react';
+import { IconPhoto } from '@tabler/icons-react';
 import { motion } from 'framer-motion';
-import Link from 'next/link';
 
 export const Introduction: FC = () => {
   return (
@@ -14,44 +13,37 @@ export const Introduction: FC = () => {
         className="mx-auto grid w-full max-w-screen-lg gap-4 px-4 md:grid-cols-2 md:gap-8 md:px-8"
       >
         <div className="order-2 space-y-4 md:order-1">
-          <h2 className="font-serif font-bold uppercase tracking-widest">
-            Dragonfly Naturist Village
-          </h2>
-          <h3 className="text-2xl font-semibold text-slate-500">
-            <svg
-              aria-hidden="true"
-              className="h-12 w-12"
-              viewBox="0 0 24 27"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
+          <div className="space-y-4 px-4 md:px-0 md:py-8">
+            <h2 className="uppercase tracking-widest text-slate-500">
+              Dragonfly Naturist Village
+            </h2>
+            <h3 className="relative p-2 text-2xl font-semibold md:p-4">
+              <svg
+                aria-hidden="true"
+                className="absolute left-0 top-0 -z-10 h-10 w-10 md:h-12 md:w-12"
+                viewBox="0 0 24 27"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                  className="fill-slate-400/25"
+                />
+              </svg>
+              Enjoy the freedom of naturism in a safe and friendly environment
+            </h3>
+            <p className="text-slate-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic unde
+              expedita dignissimos voluptatibus, nobis nam quod culpa laudantium
+              maiores minima incidunt, quis aspernatur officiis nihil fugit
+              harum, a ea necessitatibus?
+            </p>
+            <button
+              type="button"
+              className="inline-flex items-center justify-center bg-black px-4 py-2 text-lg uppercase text-white ring-1 ring-slate-900 transition-all duration-300 ease-in-out hover:bg-white hover:text-slate-900"
             >
-              <path
-                d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
-                fill="currentColor"
-              />
-            </svg>
-            Enjoy the freedom of naturism in a safe and friendly environment
-          </h3>
-          <div className="space-y-2">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic unde
-              expedita dignissimos voluptatibus, nobis nam quod culpa laudantium
-              maiores minima incidunt, quis aspernatur officiis nihil fugit
-              harum, a ea necessitatibus?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic unde
-              expedita dignissimos voluptatibus, nobis nam quod culpa laudantium
-              maiores minima incidunt, quis aspernatur officiis nihil fugit
-              harum, a ea necessitatibus?
-            </p>
-          </div>
-          <div className="flex items-center gap-2 text-slate-500">
-            <IconPhoneCall size={52} className="text-slate-400" />
-            <Link href="tel:+66958904598" className="flex flex-col">
-              <span>Reservations</span>
-              <span className="text-lg">66 95 890 4598</span>
-            </Link>
+              Book your stay
+            </button>
           </div>
         </div>
         <div className="relative order-1 grid aspect-[3/2] place-items-center bg-slate-300 md:order-2 md:aspect-auto">
