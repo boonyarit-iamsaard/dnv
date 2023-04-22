e4d32698a6f8a3f9f752511a15543ab34c7aeb56
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-22 18:33:05 +0700

    build: improve class names util

--- src/utils/class-names.ts
+++ src/utils/class-names.ts
@@ -1,1 +1,5 @@
-export const cn = (...classes: string[]) => classes.filter(Boolean).join(' ');
+import clsx, { type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+// TODO: Research how to use tailwind-merge, clsx and improve where this is used
+export const cn = (...input: ClassValue[]) => twMerge(clsx(input));
