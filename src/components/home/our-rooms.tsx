afeec36a9ceb29005f90f8ebca2de1dd5c838f45
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-25 21:03:59 +0700

    build: use swiper for rooms carousel

--- src/components/home/our-rooms.tsx
+++ src/components/home/our-rooms.tsx
@@ -1,69 +1,91 @@
-import { useEffect, useRef, useState, type FC } from 'react';
+import { type FC, type HTMLAttributes } from 'react';
 import Image from 'next/image';
 
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
+import { cva, type VariantProps } from 'class-variance-authority';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 
 import room from '@/assets/images/room.jpg';
 import { PageSection } from '@/components/common';
-import { cn } from '@/utils/class-names';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+// TODO: Move to a separate file
+export const button = cva(
+  [
+    'swiper-navigation-button',
+    'absolute inset-y-0',
+    'z-30',
+    'flex items-center',
+    'px-2',
+    'bg-transparent text-white hover:bg-black/50',
+  ],
+  {
+    variants: {
+      direction: {
+        prev: ['left-0'],
+        next: ['right-0'],
+      },
+    },
+    defaultVariants: {},
+  }
+);
+
+export interface SwiperNavigationButtonProps
+  extends HTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof button> {
+  direction: 'prev' | 'next';
+}
+
+export const SwiperNavigationButton: FC<SwiperNavigationButtonProps> = ({
+  direction,
+  className,
+  ...props
+}) => {
+  const swiper = useSwiper();
+
+  const label = direction === 'prev' ? 'Previous' : 'Next';
+
+  // TODO: Implement hidden prop
+
+  return (
+    <button
+      aria-label={label}
+      className={button({
+        className,
+        direction,
+      })}
+      onClick={() =>
+        direction === 'prev' ? swiper.slidePrev() : swiper.slideNext()
+      }
+      type="button"
+      {...props}
+    >
+      {direction === 'prev' ? <IconChevronLeft /> : <IconChevronRight />}
+    </button>
+  );
+};
 
 export const OurRooms: FC = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
-
-  const maxScrollWidth = useRef(0);
-  const carousel = useRef<HTMLDivElement>(null);
-
-  const handlePrev = () => {
-    if (activeIndex > 0) {
-      setActiveIndex(prev => prev - 1);
-    }
-  };
-
-  const handleNext = () => {
-    if (
-      carousel.current?.offsetWidth &&
-      carousel.current?.offsetWidth * activeIndex <
-        (maxScrollWidth.current ?? 0)
-    ) {
-      setActiveIndex(prev => prev + 1);
-    }
-  };
-
-  const shouldHidden = ({ direction }: { direction: 'prev' | 'next' }) => {
-    if (direction === 'prev') {
-      return activeIndex <= 0;
-    }
-
-    if (direction === 'next' && carousel.current?.offsetWidth) {
-      return (
-        carousel.current.offsetWidth * activeIndex >=
-        (maxScrollWidth.current ?? 0)
-      );
-    }
-
-    return false;
-  };
-
-  useEffect(() => {
-    const current = carousel.current;
-
-    if (current) {
-      current.scrollLeft = current.offsetWidth * activeIndex;
-    }
-
-    return () => {
-      if (current) {
-        current.scrollLeft = 0;
-      }
-    };
-  }, [activeIndex]);
-
-  useEffect(() => {
-    if (carousel.current) {
-      maxScrollWidth.current =
-        carousel.current.scrollWidth - carousel.current.offsetWidth;
-    }
-  }, []);
+  const roomTypes = [
+    {
+      type: 'Suite',
+    },
+    {
+      type: 'Mini Suite',
+    },
+    {
+      type: 'Deluxe',
+      sub: '1st Floor',
+    },
+    {
+      type: 'Deluxe',
+      sub: '2nd Floor',
+    },
+  ];
 
   return (
     <PageSection variant="white">
@@ -80,61 +102,57 @@ export const OurRooms: FC = () => {
           </div>
         </div>
 
-        <div className="relative overflow-hidden">
-          <button
-            type="button"
-            onClick={handlePrev}
-            className={cn(
-              'absolute left-1 top-1/2 z-10 -translate-y-1/2 rounded-full p-2 text-center text-white transition-all duration-300 ease-in-out hover:bg-white/20 focus:outline-none focus:ring-2 focus:ring-white',
-              {
-                hidden: shouldHidden({ direction: 'prev' }),
-              }
-            )}
+        <div className="relative">
+          <Swiper
+            spaceBetween={16}
+            slidesPerView={1.25}
+            breakpoints={{
+              600: {
+                slidesPerView: 2.25,
+              },
+              900: {
+                slidesPerView: 3.25,
+              },
+              1200: {
+                slidesPerView: roomTypes.length > 4 ? 4.25 : 4,
+              },
+            }}
+            className="relative"
           >
-            <IconChevronLeft />
-            <span className="sr-only">Prev</span>
-          </button>
-          <button
-            type="button"
-            onClick={handleNext}
-            className={cn(
-              'absolute right-1 top-1/2 z-10 -translate-y-1/2 rounded-full p-2 text-center text-white transition-all duration-300 ease-in-out hover:bg-white/20 focus:outline-none focus:ring-2 focus:ring-white',
-              {
-                hidden: shouldHidden({ direction: 'next' }),
-              }
-            )}
-          >
-            <IconChevronRight />
-            <span className="sr-only">Next</span>
-          </button>
-
-          <div
-            ref={carousel}
-            className="carousel-container relative z-0 flex touch-pan-x snap-x snap-mandatory gap-4 overflow-hidden scroll-smooth"
-          >
-            {Array.from({ length: 4 }).map((_, index) => (
-              <div
-                key={index}
-                className="relative flex aspect-[3/4] w-[calc(100%-4rem)] shrink-0 snap-start flex-col justify-end bg-gradient-to-t from-black/75 to-black/50 sm:w-[calc((100%-5rem)/2)] md:w-[calc((100%-6rem)/3)] lg:w-[calc((100%-3rem)/4)]"
-              >
-                <Image
-                  src={room}
-                  fill
-                  alt={`Room ${index + 1}`}
-                  className="-z-10 object-cover"
-                />
-                <div className="space-y-2 p-4 text-white">
-                  <h3 className="font-serif text-xl font-bold uppercase">
-                    Room {index + 1}
-                  </h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Voluptates, quia.
-                  </p>
+            {roomTypes.map(({ type, sub }, index) => (
+              <SwiperSlide key={index}>
+                <div className="group relative mb-8 flex aspect-[3/4] w-full flex-col justify-end overflow-hidden bg-gradient-to-tr from-black/75 to-black/50">
+                  <Image
+                    alt={type}
+                    src={room}
+                    fill
+                    // TODO: Research more about this
+                    sizes="(min-width: 1200px) 35vw, (min-width: 900px) 45vw, (min-width: 600px) 65vw, 100vw"
+                    className="-z-10 object-cover transition-all duration-300 ease-in-out group-hover:scale-105 group-hover:blur-sm"
+                  />
+                  <div className="space-y-2 p-4 text-white">
+                    {sub && (
+                      <span className="inline-block bg-white/20 px-2 py-1 text-sm leading-none">
+                        {sub}
+                      </span>
+                    )}
+                    <h3 className="flex items-center gap-4">
+                      <span className="block font-serif text-2xl font-bold uppercase">
+                        {type}
+                      </span>
+                    </h3>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Voluptates, quia.
+                    </p>
+                  </div>
                 </div>
-              </div>
+              </SwiperSlide>
             ))}
-          </div>
+            <SwiperNavigationButton direction="prev" className="bottom-8" />
+            <SwiperNavigationButton direction="next" className="bottom-8" />
+            {/* TODO: Imprement custom pagination */}
+          </Swiper>
         </div>
       </div>
     </PageSection>
