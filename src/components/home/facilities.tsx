1f0e0631d17298112713a2a62784a1a26785ffc3
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2023-04-27 01:29:38 +0700

    style: improve facilities section

--- src/components/home/facilities.tsx
+++ src/components/home/facilities.tsx
@@ -16,37 +16,37 @@ const facilities = [
     title: 'Swimming Pool',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, quia!',
-    icon: <IconSwimming size={64} />,
+    icon: <IconSwimming size={64} stroke={1} />,
   },
   {
     title: 'Room Service',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, quia!',
-    icon: <IconHotelService size={64} />,
+    icon: <IconHotelService size={64} stroke={1} />,
   },
   {
     title: 'Dining',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, quia!',
-    icon: <IconSoup size={64} />,
+    icon: <IconSoup size={64} stroke={1} />,
   },
   {
     title: 'Wifi',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, quia!',
-    icon: <IconWifi size={64} />,
+    icon: <IconWifi size={64} stroke={1} />,
   },
   {
     title: 'Parking',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, quia!',
-    icon: <IconParking size={64} />,
+    icon: <IconParking size={64} stroke={1} />,
   },
   {
     title: 'Gym',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, quia!',
-    icon: <IconBarbell size={64} />,
+    icon: <IconBarbell size={64} stroke={1} />,
   },
 ];
 
@@ -58,15 +58,17 @@ export const Facilities: FC = () => {
           subtitle="Dragonfly Naturist Village"
           title="Our Services and Facilities"
         />
-        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3">
+        <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-8">
           {facilities.map(({ title, description, icon }) => (
             <div
               key={title}
-              className="flex flex-col justify-end border border-slate-200 px-4 py-8 text-slate-500"
+              className="flex flex-col justify-end border border-slate-100 px-4 py-8 text-slate-500"
             >
               <div className="space-y-4">
-                <span className="text-4xl">{icon}</span>
-                <h3 className="text-2xl font-semibold">{title}</h3>
+                {icon}
+                <h3 className="text-2xl font-semibold text-slate-900">
+                  {title}
+                </h3>
                 <p>{description}</p>
               </div>
             </div>
